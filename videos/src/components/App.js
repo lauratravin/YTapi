a6bcee72430d1985abcba7a07505b956ecb3b24a
Laura Travin <laura.travin@gmail.com>
2022-02-22 19:33:12 -0500

    api test

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -1,15 +1,17 @@
 import React from "react";
 import SearchBar from "./SearchBar";
 import './App.css'
-
+import youtube from "../apis/youtube";
 
 
 
 
 class App extends React.Component {
 
-  onTermSubmit = (term) => {
-      console.log(term)
+  onTermSubmit = async term => { //this is concatenate with youtube params
+   const response = await  youtube.get('/search',  { params: { q: term} }  );
+   console.log(response) 
+  
   }
 
 
