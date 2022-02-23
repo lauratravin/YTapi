9d281315ee2b4bff41700874f28fcec09a1c8928
Laura Travin <laura.travin@gmail.com>
2022-02-23 17:59:20 -0500

    vidos list

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -2,16 +2,18 @@ import React from "react";
 import SearchBar from "./SearchBar";
 import './App.css'
 import youtube from "../apis/youtube";
-
-
+import VideoList from "./VideoList";
 
 
 class App extends React.Component {
 
+  state = {videos: []}
+
   onTermSubmit = async term => { //this is concatenate with youtube params
    const response = await  youtube.get('/search',  { params: { q: term} }  );
    console.log(response) 
-  
+   this.setState({videos: response.data.items})
+   console.log(this.state.videos)
   }
 
 
@@ -21,6 +23,8 @@ class App extends React.Component {
     return(
        <div className="ui container">
           <SearchBar onFormSubmit={this.onTermSubmit} />
+          I have {this.state.videos.length} videos.
+          <VideoList videos={this.state.videos} />
        </div>
     )
   }
---
+++ videos/src/components/VideoList.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const VideoList = (props) => {
+  return 
+      <div>VideoList</div>
+  
+};
+
+export default VideoList;
