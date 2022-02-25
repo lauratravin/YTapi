1c801b8e3af98bced9c5e4e441fa9cbdf6bd22b3
Laura Travin <laura.travin@gmail.com>
2022-02-24 21:53:05 -0500

    showing videos

--- videos/src/components/VideoList.js
+++ videos/src/components/VideoList.js
@@ -1,9 +1,15 @@
 import React from "react";
+import VideoItem from "./VideoItem";
 
-const VideoList = (props) => {
-  return 
-      <div>VideoList</div>
-  
+
+// const VideoList = (props) => { //destructuring below
+const VideoList = ({ videos}) => {
+           
+          const renderedList =  videos.map( v => 
+               {return  <VideoItem video={ v }/>}
+                    )   
+          
+  return <div>{renderedList}</div>
 };
 
 export default VideoList;
