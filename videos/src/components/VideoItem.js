ca39bbee7683dd8295655e44de2f8abe8ae030e1
Laura Travin <laura.travin@gmail.com>
2022-02-24 22:13:47 -0500

    youtube link

--- videos/src/components/VideoItem.js
+++ videos/src/components/VideoItem.js
@@ -1,7 +1,21 @@
+import { isRefObject } from "@fluentui/react-component-ref";
 import React from "react";
 
 const VideoItem = ({video}) => {
-   return<div> {video.snippet.title}</div>
+
+    console.log(video)
+    const url = "https://www.youtube.com/embed/" + video.id.videoId;
+   return <div key={video.id.videoId}> {video.snippet.title}
+        
+         <img src={video.snippet.thumbnails.medium.url}/>
+        <iframe width="560" height="315"  
+        src={url} ></iframe>
+        </div> 
 }
 
 export default VideoItem;
+
+
+{/* <iframe width="560" height="315" src="https://www.youtube.com/embed/qvAtjkQlEuQ" 
+title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; 
+clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> */}
